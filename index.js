1902e300520bf78d78631e296b3f3e1096893496
Emmanuella Ene <enemiriam522@gmail.com>
2025-01-16 11:58:40 +0100

    new updates

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ const myNavItem = document.querySelectorAll('.nav-link[data-target]'); // Target
 const sections = document.querySelectorAll('.content'); // Target sections
 
 for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener("click", ()=>{
-        pageTag.innerHTML = navLinks[i].textContent;
-    })
+  navLinks[i].addEventListener("click", () => {
+    pageTag.innerHTML = navLinks[i].textContent;
+  })
 }
 
-plusBtn.addEventListener("click", ()=>{
-    overlay.style.display = "block";
-    reservationDetails.style.display = "block";
+plusBtn.addEventListener("click", () => {
+  overlay.style.display = "block";
+  reservationDetails.style.display = "block";
 })
 
-close.addEventListener("click", ()=>{
-    overlay.style.display = "none";
-    reservationDetails.style.display = "none";
+close.addEventListener("click", () => {
+  overlay.style.display = "none";
+  reservationDetails.style.display = "none";
 })
 
 
@@ -29,16 +29,16 @@ close.addEventListener("click", ()=>{
 myNavItem.forEach(link => {
   link.addEventListener('click', event => {
     event.preventDefault(); // Prevent default behavior
-    
+
     // Get the target section ID
     const targetId = link.getAttribute('data-target');
     console.log(targetId);
-    
-    
+
+
     // Hide all sections and remove active class from nav links
     sections.forEach(section => section.classList.remove('active'));
     myNavItem.forEach(nav => nav.classList.remove('active'));
-    
+
     // Show the targeted section and mark nav link as active
     const targetSection = document.getElementById(targetId);
     if (targetSection) {
@@ -47,5 +47,5 @@ myNavItem.forEach(link => {
     link.classList.add('active');
     // console.log(sections);
 
-});
+  });
 });
